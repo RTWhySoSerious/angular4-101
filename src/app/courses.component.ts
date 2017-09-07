41b3b586cfb5321dec491deaf5312d8b45d33d26
RT Sullivan <atleast24.7@list.ru>
2017-09-07 22:22:22 +0800

    class binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -21,7 +21,7 @@ import { Component } from '@angular/core';
         <td [attr.colspan]="colSpan"></td>
       </tr>
     </table>
-    <button class="btn btn-primary">Save</button>
+    <button class="btn btn-primary" [class.active]="isActive">Save</button>
   `
 })
 export class CoursesComponent{
@@ -29,7 +29,9 @@ export class CoursesComponent{
   courses;
   imageUrl = "http://lorempixel.com/400/200";
   colSpan = 2;
+  isActive = true;
 
+  
   constructor(service: CoursesService) {// dependecies injecting
     this.courses = service.getCourses();
   }
