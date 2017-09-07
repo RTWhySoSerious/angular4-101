0351bc4e5bdd9fef77b3b854c8ad8e213899b7e5
RT Sullivan <atleast24.7@list.ru>
2017-09-07 21:52:45 +0800

    attr of DOM elements

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,21 +1,33 @@
 import { CoursesService } from './courses.service';
 
 import { Component } from '@angular/core';
-
+// <h2>{{'List of '+ title}}</h2> => is better for text
+// <img [src]="imageUrl" /> => is better for img
+// this is a one way data-binding (if smth changes in the component, it will change in DOM)
+// if smth changes in the DOM it will not change in the component
 @Component({
   selector: 'courses',
   template:`
     <h2>{{'List of '+ title}}</h2>
+    <img src="{{ imageUrl }}" />
+    <img [src]="imageUrl" />
     <ul>
       <li *ngFor="let course of courses">
         {{course}}
       </li>
     </ul>
+    <table>
+      <tr>
+        <td [attr.colspan]="colSpan"></td>
+      </tr>
+    </table>
   `
 })
 export class CoursesComponent{
   title = 'courses';
   courses;
+  imageUrl = "http://lorempixel.com/400/200";
+  colSpan = 2;
 
   constructor(service: CoursesService) {// dependecies injecting
     this.courses = service.getCourses();
