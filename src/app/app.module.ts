77061402cb27daaa23044c0c12921a979af66ae2
RT Sullivan <atleast24.7@list.ru>
2017-09-12 11:46:57 +0800

    update courses component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
-import { CoursesService } from './courses.service';
+import { CoursesService } from './courses/courses.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CoursesComponent } from "./courses.component";
-import { CourseComponent } from './course/course.component';
+import { CoursesComponent } from "./courses/courses.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    CourseComponent
   ],
   imports: [
     BrowserModule,
