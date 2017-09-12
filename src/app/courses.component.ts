70ca10a1fe39bc1e6089faca35ae78b3c2965707
RT Sullivan <atleast24.7@list.ru>
2017-09-12 11:04:19 +0800

    small fix

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,13 +2,6 @@ import { CoursesService } from './courses.service';
 
 import { Component } from '@angular/core';
 
-// <h2>{{'List of '+ title}}</h2> => is better for text
-// <img [src]="imageUrl" /> => is better for img
-// this is a one way data-binding (if smth changes in the component, it will change in DOM)
-// if smth changes in the DOM it will not change in the component
-//class binding, style binding and attr binding - it is called property binding 
-// event bubbling => child then parent
-// $event.stopPropagation() => stops event bubling 
 @Component({
   selector: 'courses',
   templateUrl: './courses.component.html',
